158b3e0363e0cb5b9bc5f93ef81350404d11330d
Deividokun <129647532+Deividokun@users.noreply.github.com>
2024-05-30 17:03:33 +0200

    Add files via upload

---
+++ src/components/show.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './show.css'
+const ShowCount = (props) => {
+  return (
+    <div>
+      <h2>tu resultado aqui: {props.count}</h2>
+    </div>
+  )
+}
+export default ShowCount
